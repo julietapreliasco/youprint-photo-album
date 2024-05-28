df5d8fb98c873984006f32b79f6ec1be5fa75b78
Paula Rinaldi <paurinaldi95@gmail.com>
2024-05-28 19:54:11 -0300

    YP-010: Created PhotoAlbumList component

---
+++ src/components/PhotoAlbumList.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react';
+import {
+  deletePhotoAlbum,
+  fetchPhotoAlbums,
+} from '../services/photoAlbumService';
+import { PhotoAlbum } from '../types';
+import { Button } from './ui/Button';
+import { useNavigate } from 'react-router-dom';
+import { useModal } from '../context/useModalHook';
+
+export const PhotoAlbumList = () => {
+  const [photos, setPhotos] = useState<PhotoAlbum[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const { openModal } = useModal();
+
+  useEffect(() => {
+    const getPhotoAlbum = async () => {
+      try {
+        const data = await fetchPhotoAlbums();
+        setPhotos(data);
+        setIsLoading(false);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError('An unknown error occurred');
+        }
+        setIsLoading(false);
+      }
+    };
+    getPhotoAlbum();
+  }, []);
+  console.log(photos);
+  console.log(error);
+  console.log(isLoading);
+
+  const handleDelete = (id: string) => {
+    openModal(
+      'Atención! Se va a borrar permanentemente la galería del cliente',
+      async () => {
+        try {
+          await deletePhotoAlbum(id);
+          setPhotos((prevPhotos) =>
+            prevPhotos.filter((photo) => photo._id !== id)
+          );
+        } catch (error) {
+          console.error('Error deleting photo album:', error);
+        }
+      }
+    );
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-5">
+      <h2 className="text-xl font-bold">Proyectos</h2>
+      {photos.map((photo) => (
+        <div
+          key={photo._id}
+          className="flex w-3/4 max-w-full flex-col items-center gap-3 rounded-xl border-2 p-5 text-center text-sm hover:bg-slate-100 md:flex-row md:justify-between md:text-base lg:w-1/2"
+        >
+          <div className="flex flex-col md:items-start">
+            <span className="font-semibold">Cliente:</span>
+            {photo.client.name && <span className="">{photo.client.name}</span>}
+            <span className="">{photo.client.phone}</span>
+          </div>
+          <div className="flex gap-3 md:self-end">
+            <Button
+              variant="PRIMARY"
+              message="Galería"
+              onClick={() => navigate(`/gallery/${photo._id}`)}
+            />
+            <Button
+              message="Eliminar"
+              onClick={() => handleDelete(photo._id)}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
